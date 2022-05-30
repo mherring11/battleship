8f7aa48022c189d167ae14ddd141a2037196e506
mherring11 <m.herring11@yahoo.com>
2022-05-30 07:17:57 -0500

    Math.floor and Math.random

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var locatione = 5;
-
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -14,7 +14,21 @@ while (isSunk == false) {
  alert("Please enter a valid cell number!");
  } else {  
  guesses = guesses + 1
-  
+
+ if (guess == location1 || guess == location2 || guess == location3) {
+     hits = hits + 1;
+
+     if (hits == 3) {
+         isSunk = true;
+         alert("You sank my battleship!");
+     }
+    } else {
+        alert("MISS");
+    }
  }
  
 }
+
+var stats = "You took " + guesses + " guesses to sink my battleship, " + "which means your shooting accuracy was " + (3/guesses);
+
+alert(stats);
